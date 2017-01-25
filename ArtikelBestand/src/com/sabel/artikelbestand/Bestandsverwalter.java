e1c31ab2ba88278c217d2bb8302b4f1121646836
go2k <m.koenig@go2k.de>
2017-01-25 11:14:19 +0100

    ....

--- ArtikelBestand/src/com/sabel/artikelbestand/Bestandsverwalter.java
+++ ArtikelBestand/src/com/sabel/artikelbestand/Bestandsverwalter.java
@@ -1,75 +1,97 @@
 package com.sabel.artikelbestand;
+
 import java.util.ArrayList;
 
 /**
- * Verwalte den Bestand eines Unternehmens.
- * Der Bestand ist beschrieben durch einen oder
- * mehrere Artikel.
+ * Verwalte den Bestand eines Unternehmens. Der Bestand ist beschrieben durch
+ * einen oder mehrere Artikel.
  * 
- * @author (Ihr Name) 
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Bestandsverwalter
-{
-    // Das Lager mit den Artikeln
-    private ArrayList<Artikel> lager;
+public class Bestandsverwalter {
+	// Das Lager mit den Artikeln
+	private ArrayList<Artikel> lager;
 
-    /**
-     * Initialisiere den Bestandsverwalter.
-     */
-    public Bestandsverwalter()
-    {
-        lager = new ArrayList<Artikel>();
-    }
+	/**
+	 * Initialisiere den Bestandsverwalter.
+	 */
+	public Bestandsverwalter() {
+		lager = new ArrayList<Artikel>();
+	}
 
-    /**
-     * Führe einen neuen Artikel im Lager ein.
-     * @param artikel Der Artikel, der neue eingeführt werden soll.
-     */
-    public void neuerArtikel(Artikel artikel)
-    {
-        lager.add(artikel);
-    }
-    
-    /**
-     * Nimm eine Lieferung eines Artikels in das Lager auf.
-     * Erhöhe den Lagerbestand um die angegebene Menge.
-     * @param nummer Die Artikelnummer des Artikels.
-     * @param menge Die angelieferte Menge.
-     */
-    public void aufnehmen(int nummer, int menge)
-    {
-    }
-    
-    /**
-     * Versuche einen Artikel mit der angegebenen Nummer im
-     * Bestand zu finden.
-     * @param nummer Die Nummer des zu findenden Artikels.
-     * @return den gefundenen Artikel oder null, falls kein
-     *         passender Artikel gefunden wird.
-     */
-    public Artikel findeArtikel(int nummer)
-    {
-        return null;
-    }
-    
-    /**
-     * Finde einen Artikel mit der angegebenen Nummer und
-     * liefere seine aktuelle Menge im Bestand.
-     * Wenn die Nummer auf keinen Artikel passt, wird
-     * Null zurückgeliefert.
-     * @param nummer Die Nummer des Artikels.
-     * @return die Menge des Artikels im Bestand.
-     */
-    public int mengeImBestand(int nummer)
-    {
-        return 0;
-    }
+	/**
+	 * Führe einen neuen Artikel im Lager ein.
+	 * 
+	 * @param artikel
+	 *            Der Artikel, der neue eingeführt werden soll.
+	 */
+	public void neuerArtikel(Artikel artikel) {
+		lager.add(artikel);
+	}
 
-    /**
-     * Informationen über alle Artikel ausgeben.
-     */
-    public void alleArtikelAnzeigen()
-    {
-    }
+	/**
+	 * Nimm eine Lieferung eines Artikels in das Lager auf. Erhöhe den
+	 * Lagerbestand um die angegebene Menge.
+	 * 
+	 * @param nummer
+	 *            Die Artikelnummer des Artikels.
+	 * @param menge
+	 *            Die angelieferte Menge.
+	 */
+	public void aufnehmen(int nummer, int menge) {
+		
+		
+
+	}
+
+	/**
+	 * Versuche einen Artikel mit der angegebenen Nummer im Bestand zu finden.
+	 * 
+	 * @param nummer
+	 *            Die Nummer des zu findenden Artikels.
+	 * @return den gefundenen Artikel oder null, falls kein passender Artikel
+	 *         gefunden wird.
+	 */
+	public Artikel findeArtikel(int nummer) {
+
+		for (Artikel artikel : lager) {
+			if (artikel.gibNummer() == nummer) {
+				return artikel;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Finde einen Artikel mit der angegebenen Nummer und liefere seine aktuelle
+	 * Menge im Bestand. Wenn die Nummer auf keinen Artikel passt, wird Null
+	 * zurückgeliefert.
+	 * 
+	 * @param nummer
+	 *            Die Nummer des Artikels.
+	 * @return die Menge des Artikels im Bestand.
+	 */
+	public int mengeImBestand(int nummer) {
+
+		for (Artikel artikel : lager) {
+			if (artikel.gibNummer() == nummer) {
+				return artikel.gibBestand();
+			}
+		}
+
+		return 0;
+	}
+
+	/**
+	 * Informationen über alle Artikel ausgeben.
+	 */
+	public void alleArtikelAnzeigen() {
+
+		for (Artikel artikel : lager) {
+			System.out.println(artikel.toString());
+		}
+
+	}
 }
